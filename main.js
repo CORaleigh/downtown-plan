40bc3f4163ceb9a6e0578f2ebb3224b722d121a5
justingreco <justin.greco@raleighnc.gov>
2016-10-23 11:33:00 -0400

    added address search

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 var themeLyr = null,
     areaLyr = null,
     themes = [0, 1, 2, 3],
-    areas = [0, 1, 2, 3, 4];
+    areas = [0, 1, 2, 3, 4],
+    map = null,
+    view = null;
 
 
-document.documentElement.addEventListener('touchstart', function (event) {
-  if (event.touches.length > 1) {
-    event.preventDefault();
-  }
+
+
+document.documentElement.addEventListener('touchstart', function(event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
 }, false);
 document.documentElement.querySelector('.mdl-layout__content').addEventListener('touchmove', function(e) {
-        e.preventDefault();
+    e.preventDefault();
 
 }, false);
 require([
@@ -30,7 +34,7 @@ require([
      * To load a WebMap from an on-premise portal, set the portal
      * url in esriConfig.portalUrl.
      ************************************************************/
-    var map = new WebMap({
+    map = new WebMap({
         portalItem: { // autocasts as new PortalItem()
             id: "05e67378902b4128b31ce7f07f3808a7"
         }
@@ -39,7 +43,7 @@ require([
     /************************************************************
      * Set the WebMap instance to the map property in a MapView.
      ************************************************************/
-    var view = new MapView({
+    view = new MapView({
         map: map,
         container: "map"
     });
@@ -56,9 +60,9 @@ require([
         });
         map.basemap.baseLayers = [];
         var tileLyr = new VectorTileLayer({
-          url: "https://www.arcgis.com/sharing/rest/content/items/3981b4e8cabb4b0fb6d4a8c94379532b/resources/styles/root.json"
+            url: "https://www.arcgis.com/sharing/rest/content/items/3981b4e8cabb4b0fb6d4a8c94379532b/resources/styles/root.json"
         });
-        map.add(tileLyr, 0);          
+        map.add(tileLyr, 0);
     });
 
 });
@@ -86,4 +90,76 @@ function filterArea(element, area) {
         element.querySelector('svg').classList.remove('unselected');
     }
     areaLyr.definitionExpression = "Name in (" + areas.toString() + ")";
+}
+
+function searchEntered(element) {
+    var node = document.getElementById('list');
+
+
+    var xmlhttp;
+    // compatible with IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            while (node.hasChildNodes()) {
+                node.removeChild(node.lastChild);
+            }
+            document.getElementById("list").style.display = 'block';
+
+            var data = JSON.parse(xmlhttp.responseText);
+            data.features.forEach(function(d) {
+                document.getElementById('list')
+                    .insertAdjacentHTML('beforeend', '<li onclick="itemSelected(event)" class="mdl-list__item"><span data-x="' + d.geometry.x + '" data-y="' + d.geometry.y + '" class="address mdl-list__item-primary-content">' + d.attributes.ADDRESS + '</span></li>');
+            });
+        }
+    }
+    xmlhttp.open("GET", "https://maps.raleighnc.gov/arcgis/rest/services/Addresses/MapServer/0/query?f=json&outSR=4326&returnGeometry=true&outFields=ADDRESS&orderByFields=ADDRESS&resultRecordCount=5&where=ADDRESSU LIKE '" + element.value.toUpperCase() + "%'", true);
+    if (element.value.length > 3)
+        xmlhttp.send();
+}
+
+document.addEventListener('click', hideList);
+document.getElementById('inputHolder').addEventListener("transitionend", inputResize, false);
+function itemSelected(event) {
+    var x = null, y = null;
+    if (event.target.children.length > 0) {
+        x = event.target.children[0].getAttribute('data-x');
+        y = event.target.children[0].getAttribute('data-y');        
+    } else {
+        x = event.target.getAttribute('data-x');
+        y = event.target.getAttribute('data-y'); 
+    }
+    document.getElementById("search").value = '';
+    document.getElementById("title").style.display = 'block';
+    document.getElementById("inputHolder").style.maxWidth = '.1px';
+  
+  // -> and re-adding the class
+    zoomToLocation([parseFloat(x),parseFloat(y)], 18);
+};
+
+function searchClicked() {
+    if (document.getElementById("title").style.display === 'block' || document.getElementById("title").style.display === '') {
+        document.getElementById("title").style.display = 'none';
+        document.getElementById("inputHolder").style.maxWidth = '600px';
+    } else {
+        
+    }
+};
+
+function zoomToLocation(center, zoom) {
+    view.goTo({
+        center: center,
+        zoom: zoom
+    });
+};
+
+function inputResize(event) {
+    if (event.target.clientWidth === 0) {
+        document.getElementById("title").style.display = 'block';
+    }
+}
+
+function hideList(event) {
+    if (event.target.id != "list")
+        document.getElementById("list").style.display = 'none';
 }
